6da281a1b89279bc923ed59a59056297012642d3
Jatin Gupta <jatin.gupta@kgpian.iitkgp.ac.in>
2022-08-12 16:01:00 +0530

    .

---
+++ A2/Q2_Grp_69.s
@@ -0,0 +1,39 @@
+    .globl main
+    .data
+
+array: .space 40
+newline:
+    .asciiz "\n"
+prompt1: 
+    .asciiz "Enter the array elements: "
+
+    .text
+main:
+    li $v0, 4
+    la $a0, prompt1
+    syscall
+
+    j read_input
+
+sort_array:
+    la $t1, array
+    li $s1, 10
+
+first_loop:
+    beq $s1, $s2, 
+
+read_input:
+    addi $sp, $sp, -4
+    li $v0, 5
+    syscall
+
+    mul $t4, $t4, 4
+    beq $t0, $t4, function_end
+
+    sw $v0, array($t0)
+    addi $t0, $t0,  4
+    addi $sp, $sp, 4
+    j read_input
+
+function_end:
+    jr $ra
