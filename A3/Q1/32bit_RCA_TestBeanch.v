a1a851b90be81089dbbf6986a2a10cefcf5e9d93
Jatin Gupta <jatin.gupta@kgpian.iitkgp.ac.in>
2022-08-25 22:21:36 +0530

    comments

--- A3/Q1/32bit_RCA_TestBeanch.v
+++ A3/Q1/32bit_RCA_TestBeanch.v
@@ -29,15 +29,15 @@ module RCA32_TB;
             // Test cases
             // 32-bit unsigned numbers can vary from [0, 4294967295]
             // Output can range from [0, 8589934591]
-            A = 32'd4294967290; B = 32'd4294967294; cin = 1'd0;       
+            A = 32'd4294967290; B = 32'd4294967294; cin = 1'd0;     //sum = 32'd4294967288, cout = 32'd1    
             #100;
-            A = 32'd0; B = 32'd4294967295; cin = 1'd0;        
+            A = 32'd1; B = 32'd4294967295; cin = 1'd0;              //sum = 32'd0, cout = 32'd1
             #100;
-            A = 32'd1005; B = 32'd69; cin = 1'd1;          
+            A = 32'd1005; B = 32'd69; cin = 1'd1;                   //sum = 32'd1075, cout = 32'd0
             #100;
-            A = 32'd151242; B = 32'd53831224; cin = 1'd1;        
+            A = 32'd151242; B = 32'd53831224; cin = 1'd1;           //sum = 32'd53982466, cout = 32'd0
             #100;
-            A = 32'd501; B = 32'd5002423; cin = 1'd0;          
+            A = 32'd501; B = 32'd5002423; cin = 1'd0;               //sum = 32'd5002924, cout = 32'd0
         end
 
 endmodule
