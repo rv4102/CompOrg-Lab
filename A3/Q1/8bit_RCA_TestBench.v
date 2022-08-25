5cda96f2a7907abe200711fc007b3c75045b5851
Rushil Venkateswar <rushilv14@gmail.com>
2022-08-25 11:43:53 +0530

    RCA8 TB

--- A3/Q1/8bit_RCA_TestBench.v
+++ A3/Q1/8bit_RCA_TestBench.v
@@ -11,37 +11,33 @@
 
 module RCA8_TB;
     // Inputs
-    reg in1;
-    reg in2;
+    reg[7:0] A;
+    reg[7:0] B;
     reg cin;
 
     // Outputs
-    wire sum;
-    wire carry;
+    wire[7:0] sum;
+    wire cout;
 
-    full_addr F1(.in1(in1), .in2(in2), .cin(cin), 
-                .sum(sum), .carry(carry));
+    RCA_8 A1(.A(A), .B(B), .cin(cin), 
+                .sum(sum), .cout(cout));
     
     initial 
         begin
-            $monitor ("in1 = %d, in2 = %d, cin = %d, sum = %d, carry = %d\n", in1, in2, cin, sum, carry);
+            $monitor ("A = %d, B = %d, cin = %d, sum = %d, cout = %d\n", A, B, cin, sum, cout);
 
             // Test cases
-            in1 = 0; in2 = 0; cin = 0;
+            // 8-bit unsigned numbers can vary from [0, 256]
+            // Output can range from [0, 512]
+            A = 8'd120; B = 8'd240; cin = 1'd0;     // sum = 8'd104, cout = 1'd1
             #100;
-            in1 = 0; in2 = 0; cin = 1;
+            A = 8'd169; B = 8'd256; cin = 1'd1;     // sum = 8'd170, cout = 1'd1
             #100;
-            in1 = 0; in2 = 1; cin = 0;
+            A = 8'd53; B = 8'd250; cin = 1'd0;      // sum = 8'd47, cout = 1'd1
             #100;
-            in1 = 0; in2 = 1; cin = 1;
+            A = 8'd1; B = 8'd50; cin = 1'd1;        // sum = 8'd50, cout = 1'd0
             #100;
-            in1 = 1; in2 = 0; cin = 0;
-            #100;
-            in1 = 1; in2 = 0; cin = 1;
-            #100;
-            in1 = 1; in2 = 1; cin = 0;
-            #100;
-            in1 = 1; in2 = 1; cin = 1;
+            A = 8'd50; B = 8'd100; cin = 1'd0;      // sum = 8'd150, cout = 1'd0
         end
 
 endmodule
