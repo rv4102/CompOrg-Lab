0691dfe017798531d34fdddad47741b8d2e6a6b3
Jatin Gupta <jatin.gupta@kgpian.iitkgp.ac.in>
2022-08-30 20:25:30 +0530

    module files

---
+++ A4/bit_4_augment.v
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+
+//////////////////////////////////////////////////////////////////////////////////
+// Assignment 3
+// Question 1
+// Group 69
+// Team Member 1: Rushil Venkateswar (20CS30045)
+// Team Member 2: Jatin Gupta (20CS10087)
+// SEM 5 (Autumn 2022-23)
+//////////////////////////////////////////////////////////////////////////////////
+
+module bit_4_augment (input [3:0] A, input [3:0] B, input cin, 
+                    output [3:0] sum, output p, output g, output cout);
+    
+    wire [3:0] P;
+    wire [3:0] G;
+    wire [3:0] C;
+
+    assign G = A & B;
+    assign P = A ^ B;
+
+    assign C[0] = cin;
+    
+    assign C[1] = (P[0] & C[0]);
+    assign C[1] = G[0] | C[1];
+    
+    assign C[2] = (P[1] & G[0]);
+    assign C[2] = C[2] | (P[1] & P[0] & C[0]);
+    assign C[2] = C[2] | G[1];
+    
+    assign C[3] = (P[2] & G[1]); 
+    assign C[3] = C[3] | (P[2] & P[1] & G[0]);
+    assign C[3] = C[3] | (P[2] & P[1] & P[0] & C[0]);
+    assign C[3] = C[3] | G[2];
+    
+    assign cout = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) | (P[3] & P[2] & P[1] & P[0] & carry[0]);
+
+    assign sum = P^C;
+    
+    assign p = P[3] & P[2] & P[1] & P[0];
+	assign g = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]);
+
+    
+
+endmodule
---
+++ A4/lcu.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+
+//////////////////////////////////////////////////////////////////////////////////
+// Assignment 3
+// Question 1
+// Group 69
+// Team Member 1: Rushil Venkateswar (20CS30045)
+// Team Member 2: Jatin Gupta (20CS10087)
+// SEM 5 (Autumn 2022-23)
+//////////////////////////////////////////////////////////////////////////////////
+
+module lcu (input [3:0] P, input [3:0] G,
+                output [3:0] c, output cout, output p, output q)
+
+    assign c[0] = cin;
+    assign c[1] = G[0] | (P[0] & c[0]);
+    assign c[2] = G[1] | (P[1] & G[0]) | (P[1] & P[0] & c[0]);
+    assign c[3] = G[2] | (P[2] & G[1]) | (P[2] & P[1] & G[0]) | (P[2] & P[1] & P[0] & c[0]);
+    assign cout = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) | (P[3] & P[2] & P[1] & p[0] & c[0]);
+
+    assign p = P[3] & P[2] & P[1] & P[0];
+    assign g = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]);
+
+endmodule   
