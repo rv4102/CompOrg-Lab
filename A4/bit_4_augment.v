e715b1d9e17ed6ba1dd21d46f5b224ba5a4f408c
jatin0101 <86053879+jatin0101@users.noreply.github.com>
2022-08-31 13:40:13 +0530

    Update bit_4_augment.v

--- A4/bit_4_augment.v
+++ A4/bit_4_augment.v
@@ -21,23 +21,19 @@ module bit_4_augment (input [3:0] A, input [3:0] B, input cin,
 
     assign C[0] = cin;
     
-    assign C[1] = (P[0] & C[0]);
-    assign C[1] = G[0] | C[1];
+    assign C[1] = G[0] | (P[0] & C[0]);
     
-    assign C[2] = (P[1] & G[0]);
-    assign C[2] = C[2] | (P[1] & P[0] & C[0]);
-    assign C[2] = C[2] | G[1];
+    assign C[2] = G[1] | (P[1] & G[0]) | (P[1] & P[0] & C[0]);;
     
-    assign C[3] = (P[2] & G[1]); 
-    assign C[3] = C[3] | (P[2] & P[1] & G[0]);
-    assign C[3] = C[3] | (P[2] & P[1] & P[0] & C[0]);
-    assign C[3] = C[3] | G[2];
+    assign C[3] = G[2] |(P[2] & G[1]) | (P[2] & P[1] & G[0]) | (P[2] & P[1] & P[0] & C[0]); 
+    
+    //assign cout = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) | (P[3] & P[2] & P[1] & P[0] & C[0]);
     
     //assign cout = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) | (P[3] & P[2] & P[1] & P[0] & carry[0]);
 
     assign sum = P^C;
     
     assign p = P[3] & P[2] & P[1] & P[0];
-	assign g = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]);
+	 assign g = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]);
 
 endmodule
