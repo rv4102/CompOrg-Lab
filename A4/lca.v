2196674ecb5369fd7889bc71551f703d98359ae4
jatin0101 <86053879+jatin0101@users.noreply.github.com>
2022-08-31 13:41:17 +0530

    Update lca.v

--- A4/lca.v
+++ A4/lca.v
@@ -9,14 +9,13 @@
 // SEM 5 (Autumn 2022-23)
 //////////////////////////////////////////////////////////////////////////////////
 
-module lca (input [3:0] P, input [3:0] G,
-                output [3:0] c, output p, output q)
+module lca (input [3:0] P, input [3:0] G, input cin,
+                output [3:0] c, output p, output g);
 
-    assign c[0] = cin;
-    assign c[1] = G[0] | (P[0] & c[0]);
-    assign c[2] = G[1] | (P[1] & G[0]) | (P[1] & P[0] & c[0]);
-    assign c[3] = G[2] | (P[2] & G[1]) | (P[2] & P[1] & G[0]) | (P[2] & P[1] & P[0] & c[0]);
-    assign cout = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) | (P[3] & P[2] & P[1] & p[0] & c[0]);
+    assign c[0] = G[0] | (P[0] & cin);
+    assign c[1] = G[1] | (P[1] & G[0]) | (P[1] & P[0] & cin);
+    assign c[2] = G[2] | (P[2] & G[1]) | (P[2] & P[1] & G[0]) | (P[2] & P[1] & P[0] & cin);
+    assign c[3] = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) | (P[3] & P[2] & P[1] & P[0] & cin);
 
     assign p = P[3] & P[2] & P[1] & P[0];
     assign g = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]);
